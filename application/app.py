fafd2699df2e85697f739ea7bdbaae6414e7fcaa
Andriy Boreyko <4boreyko@gmail.com>
2016-10-28 11:36:50 +0300

    Add AlchemyDumps support

--- application/app.py
+++ application/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
+from flask_alchemydumps import AlchemyDumps, AlchemyDumpsCommand
 from .settings import Configuration
 
 
@@ -12,3 +13,9 @@ migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+# init Alchemy Dumps
+# manage.py alchemydumps create
+# manage.py alchemydumps restore -d <id>
+alchemydumps = AlchemyDumps(app, db)
+manager.add_command('alchemydumps', AlchemyDumpsCommand)
